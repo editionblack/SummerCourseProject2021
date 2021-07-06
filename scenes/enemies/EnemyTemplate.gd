d3106ee77080b5f8abba42f006f531aadf1adf2b
editionblack <linusepichagstrand@gmail.com>
2021-07-06 14:42:14 +0200

    made the enemies a little more spinny if they get close

--- scenes/enemies/EnemyTemplate.gd
+++ scenes/enemies/EnemyTemplate.gd
@@ -3,7 +3,6 @@ extends KinematicBody2D
 var stats
 var targets = []
 var color = null
-var accent_color = null
 
 var can_move = true
 var is_stunned = false
@@ -11,10 +10,24 @@ var nav2d = null
 var path = []
 var world = null
 
+var velocity = Vector2()
+
 func _ready():
 	$Sprite.modulate = color
 	world = get_tree().get_root().get_node("World")
 	nav2d = world.get_node("Navigation2D")
+
+
+func _process(_delta):
+	# if we are stunned and cant move there is nothing to do this frame.
+	# attacks or such should still process if we aren't stunned but can't move.
+	# subject to change but it's fine for now.
+	if is_stunned:
+		return
+	attack()
+	if !can_move:
+		return
+	move()
 
 func on_hit(damage):
 	stats["health"] -= damage
@@ -27,3 +40,8 @@ func drop_item():
 	item.global_position = global_position
 	get_parent().call_deferred("add_child", item)
 
+func move():
+	pass
+
+func attack():
+	pass
--- scenes/enemies/chaser/Chaser.gd
+++ scenes/enemies/chaser/Chaser.gd
@@ -1,21 +1,41 @@
 extends "res://scenes/enemies/EnemyTemplate.gd"
 
+var rotation_speed = 5.0
+var min_rotation_speed = 5.0
+var max_rotation_speed = 20.0
+
 func _ready():
 	._ready()
 	$DamageTimer.wait_time = stats["damage_frequency"]
 
-func _process(_delta):
-	if is_stunned or !can_move:
-		return
+func attack():
+	if global_position.distance_to(world.get_player().global_position) < 150:
+		rotation_speed = lerp(rotation_speed, max_rotation_speed, 0.25)
+	else:
+		rotation_speed = lerp(rotation_speed, min_rotation_speed, 0.25)
+
+func move():
 	var player = world.get_player()
 	var direction = Vector2(0,0)
+	
+	# raycast to the player position and get the first thing that the raycast collides with
 	$RayCast2D.cast_to = player.global_position - global_position
 	$RayCast2D.force_raycast_update()
 	var raycast_collision = $RayCast2D.get_collider()
+	
+	# if the raycast cant directly hit the player we use the navigation2d node to get a simple path
+	# that the enemy will try to follow instead. if no path to the player is possible we simply remove
+	# the enemy for the time being as this would indicate that the enemey has been spawned somewhere
+	# it shouldn't have. If a path is available the enemy will move towards the first position in the array
+	# until its close enough that the position should be removed.
+	# if the raycast does hit the player the enemy just moves straight towards it.
 	if raycast_collision != player:
 		if path.size() < 1:
 			path = nav2d.get_simple_path(global_position, player.global_position, false)
-		if global_position.distance_to(path[0]) < 5:
+			if path.size() < 1:
+				queue_free()
+				return
+		if global_position.distance_to(path[0]) < 1:
 			path.remove(0)
 		else:
 			direction = (path[0] - global_position).normalized()
@@ -23,11 +43,11 @@ func _process(_delta):
 		path = []
 		direction = (player.global_position - global_position).normalized()
 	
-	var _velocity = move_and_slide(direction * stats["movement_speed"])
+	velocity = move_and_slide(direction * stats["movement_speed"])
 
 func _physics_process(delta):
 	if !is_stunned:
-		$Sprite.rotation += 5 * delta
+		$Sprite.rotation += rotation_speed * delta
 
 func on_hit(damage):
 	damage_taken_effect()

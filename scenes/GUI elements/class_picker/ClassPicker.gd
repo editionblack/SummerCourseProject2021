2a7659894dca80e6602f2fa5d4c45acf859f9be0
editionblack <linusepichagstrand@gmail.com>
2021-07-27 12:38:57 +0200

    capitalized class names in class picker

--- scenes/GUI elements/class_picker/ClassPicker.gd
+++ scenes/GUI elements/class_picker/ClassPicker.gd
@@ -4,13 +4,14 @@ signal class_chosen(player_class)
 
 onready var item_list = $Panel/ItemList
 onready var button = $Panel/Button
-
+var classes = []
 
 func _ready():
 	var data = PlayerClassHandler.get_class_data()
 	var i = 0
 	for player_class in data.keys():
-		item_list.add_item(player_class, load(data[player_class]["icon"]), true)
+		classes.append(player_class)
+		item_list.add_item(player_class.capitalize(), load(data[player_class]["icon"]), true)
 		item_list.set_item_icon_modulate(i, data[player_class]["icon_color"])
 		item_list.set_item_tooltip_enabled(i, false)
 		i += 1
@@ -19,7 +20,7 @@ func _on_ItemList_item_selected(_index):
 	button.disabled = false
 
 func _on_Button_pressed():
-	var selected_class = item_list.get_item_text(item_list.get_selected_items()[0])
+	var selected_class = classes[item_list.get_selected_items()[0]]
 	emit_signal("class_chosen", selected_class)
 
 func _on_ItemList_nothing_selected():
@@ -31,5 +32,5 @@ func reset_item_list():
 	button.disabled = true
 
 func _on_ItemList_item_activated(_index):
-	var selected_class = item_list.get_item_text(item_list.get_selected_items()[0])
+	var selected_class = classes[item_list.get_selected_items()[0]]
 	emit_signal("class_chosen", selected_class)

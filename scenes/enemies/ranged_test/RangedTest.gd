4c1c4afc71088f77118c000d04291c5045e6800d
editionblack <linusepichagstrand@gmail.com>
2021-07-29 01:23:00 +0200

    made ranged enemies get a higher accuracy when the player is close

--- scenes/enemies/ranged_test/RangedTest.gd
+++ scenes/enemies/ranged_test/RangedTest.gd
@@ -7,15 +7,17 @@ func _ready():
 	call_deferred("add_child", ability)
 
 func attack():
-	if is_player_in_sight() and distance_to_player() < 500:
-		ability.use_ability((player.global_position - global_position).normalized().rotated(deg2rad(randi() % 21 - 10)))
+	if is_player_in_sight() and distance_to_player() < stats["range"]:
+		var accuracy = distance_to_player() / stats["range"]
+		var deviation = (randi() % 101 - 50) * accuracy
+		ability.use_ability((player.global_position - global_position).normalized().rotated(deg2rad(deviation)))
 
 func move():
 	var direction = Vector2()
 	if !is_player_in_sight() or distance_to_player() > 300:
 		direction = pathfind_direction_to_player()
 	else:
-		if distance_to_player() < 280:
+		if distance_to_player() < 200:
 			direction = (player.global_position - global_position).normalized() * -1
 	velocity = move_and_slide(direction * stats["movement_speed"])
 
